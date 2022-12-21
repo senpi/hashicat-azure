12264911424c0ef268532a7be1d6f964d258a821
Yuichiro Mitsumori <io.ti.sento@gmail.com>
2022-12-21 08:38:27 +0000

    Added the second tag retry2

--- main.tf
+++ main.tf
@@ -16,8 +16,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
   location = var.location
 
   tags = {
-    Department = "devops"
-    Billable = "true"
+    environment = "Production"
   }
 }
 
@@ -136,7 +135,10 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {    
+    Department = "devops"
+    Billable = "true"
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
